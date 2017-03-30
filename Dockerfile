b3f0b8b12953b15cd5753fbf312643332f51b572
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2017-03-30 14:28:20 +0900

    Insert

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ ARG LOCAL_BUILD=
 ENV BUNDLE_PATH=/app/vendor/bundle \
   BUNDLE_JOBS=4
 
+RUN apk add --no-cache build-base mysql-dev
+
 WORKDIR /app
 
 COPY Gemfile Gemfile.lock ./
--- worker.rb
+++ worker.rb
@@ -1,10 +1,17 @@
 require "bundler"
 Bundler.require
 
+ActiveRecord::Base.establish_connection(ENV["DATABASE_URL"])
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class Message < ActiveRecord::Base
+end
+
 class ExampleWorker
   include Sidekiq::Worker
 
   def perform(message)
-    puts message
+    m = Message.create!(body: message)
+    puts m.body
   end
 end
